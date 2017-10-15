530a525a49b10bc87704949915e73dd57d3d9ab8
John Carr <john.carr@unrouted.co.uk>
2017-10-15 23:17:26 +0100

    Add missing test.py

---
+++ agentless/tests/test_models.py
@@ -0,0 +1,23 @@
+import unittest
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from agentless.models import PrivateKey
+from agentless.crypto import generate_private_key
+
+
+class TestModel(unittest.TestCase):
+
+    def test_sign(self):
+        private_key = PrivateKey(name='my-test-key', private_key=generate_private_key())
+        data = b"hello world"
+
+        signature = private_key.sign(data)
+
+        private_key.pkey.public_key().verify(
+            signature,
+            data,
+            padding.PKCS1v15(),
+            hashes.SHA1()
+        )
